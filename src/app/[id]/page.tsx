1647c31ab128d7fdfafec971942a93ff824ffa48
Greg Frost <gregfrost@Gregs-MBP.lan>
2023-09-11 21:41:39 +0100

    feat: add suspense for mdx component

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -4,6 +4,7 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 import ProfileCard from "@/app/components/ProfileCard";
 import Article from "@/app/components/Article";
 import MarkdownImage from "@/app/components/MarkdownImage";
+import { Suspense } from "react";
 
 const components = { MarkdownImage };
 
@@ -18,13 +19,15 @@ const Page = ({ params }: { params: { id: string } }) => {
 
   return (
     <Grid container sx={{ flexGrow: 1, marginTop: "6rem" }}>
-      <Grid md>{/* <ProfileCard author={data.author} /> */}</Grid>
-      <Grid sm={12} md={8}>
+      <Grid item md></Grid>
+      <Grid item sm={12} md={8}>
         <Article data={data}>
-          <MDXRemote source={content} components={components} />
+          <Suspense fallback={<>Still loading...</>}>
+            <MDXRemote source={content} components={components} />
+          </Suspense>
         </Article>
       </Grid>
-      <Grid md></Grid>
+      <Grid item md></Grid>
     </Grid>
   );
 };
