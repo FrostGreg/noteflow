a833f0991f16802511a315c09b5830cfe2fc602d
Greg Frost <gregfrost@Gregs-MBP.lan>
2023-09-26 16:57:31 +0100

    remove lazy loading from SSG

--- src/app/(notes)/[id]/page.tsx
+++ src/app/(notes)/[id]/page.tsx
@@ -1,12 +1,10 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
 import Note from "@/app/components/Note";
-import { Suspense } from "react";
-import dynamic from "next/dynamic";
+import MarkdownImage from "@/app/components/MarkdownImage";
 
 // Can use service since it's a server component
 import { readNoteData, readNotesIds } from "@/utils/service";
 
-const MarkdownImage = dynamic(() => import("@/app/components/MarkdownImage"));
 const components = { MarkdownImage };
 
 export const dynamicParams = false;
@@ -19,13 +17,12 @@ export async function generateStaticParams() {
   return temp;
 }
 
+// Ignore Invalid prop `children` supplied to `ForwardRef(Box)` error in dev it's built using SSG
 const Page = async ({ params }: { params: { id: string } }) => {
   const { data, content } = readNoteData(params.id);
   return (
     <Note data={data} content={content}>
-      <Suspense fallback={<>Loading..</>}>
-        <MDXRemote source={content} components={components} />
-      </Suspense>
+      <MDXRemote source={content} components={components} />
     </Note>
   );
 };
