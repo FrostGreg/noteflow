6fe7f051a76111e0b6b20b653ee8bed7f008e2a0
Greg Frost <gregfrost@Gregs-MBP.lan>
2023-09-26 22:13:41 +0100

    fix: fetch relative path

--- src/utils/orch.ts
+++ src/utils/orch.ts
@@ -1,10 +1,10 @@
 export async function getNoteIDs(tag?: string) {
   const params = tag ? `?tag=${tag}` : "";
-  const res = await fetch(`http://localhost:3000/api/note-ids${params}`);
+  const res = await fetch(`/api/note-ids${params}`);
   return res.json();
 }
 
 export async function getNoteData(id: string) {
-  const res = await fetch(`http://localhost:3000/api/note-data?id=${id}`);
+  const res = await fetch(`/api/note-data?id=${id}`);
   return res.json();
 }
