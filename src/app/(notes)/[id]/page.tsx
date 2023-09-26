a79254cfb26b139b1b04eaca7476d70a1916894e
Greg Frost <gregfrost@Gregs-MBP.lan>
2023-09-26 15:55:56 +0100

    fix: call service in generateStaticParams

--- src/app/(notes)/[id]/page.tsx
+++ src/app/(notes)/[id]/page.tsx
@@ -2,23 +2,24 @@ import { MDXRemote } from "next-mdx-remote/rsc";
 import Note from "@/app/components/Note";
 import { Suspense } from "react";
 import dynamic from "next/dynamic";
-import { getNoteIDs, getNoteData } from "@/utils/orch";
+import { getNoteData } from "@/utils/orch";
+import { readNotesIds } from "@/utils/service";
 
 const MarkdownImage = dynamic(() => import("@/app/components/MarkdownImage"));
-
 const components = { MarkdownImage };
 
 export const dynamicParams = false;
 
 export async function generateStaticParams() {
-  const noteIDs = (await getNoteIDs()) as string[];
-
+  // uses service not fetch since it's run on the server
+  const noteIDs = readNotesIds();
   return noteIDs.map((id) => {
     id;
   });
 }
 
 const Page = async ({ params }: { params: { id: string } }) => {
+  //
   const { data, content } = await getNoteData(params.id);
   return (
     <Note data={data} content={content}>
